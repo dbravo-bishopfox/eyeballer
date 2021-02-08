8aabc1b09034a84a999690d33772f0a2010b78ba
David <dbravo@bishopfox.com>
2021-02-08 17:09:48 -0500

    saving progress

---
+++ webui/src/components/ClassifyButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class ClassifyButton extends React.Component {
+    // for each object classify and construct classifiedObject
+    // populate classifiedObjects
+    render() {
+        const objects = this.props.objects
+        return <div></div>
+    }
+}
+
+export default ClassifyButton;
---
+++ webui/src/components/PresentResults.js
@@ -0,0 +1,14 @@
+//iteratively present images in grid
+//include attributes such as classification & name in object
+// 3 per page
+import React from 'react';
+
+class PresentResults extends React.Component {
+    render() {
+        console.log("Hello!")
+        return <div>hello</div>
+    }
+    
+}
+
+export default PresentResults;
